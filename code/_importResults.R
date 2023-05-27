347adc29b4a3701acc5391989542b6b8b515bff3
Bruno <bruno.gauthier@gmail.com>
2023-05-26 23:06:05 -0400

    Création liste coureurs et equipes, ref #50

--- code/_importResults.R
+++ code/_importResults.R
@@ -28,18 +28,17 @@ system(glue('unrar e {here("results","rar", "StageRace.rar")} {here("results","u
 
 # Import des fichiers CSV
 
-path <- here("results","unrar")
-
 # Lire les fichiers csv dans le dossier unrar
-csv_files <- list.files(path = path,
+csv_files <- list.files(path = here("results","unrar"),
                              pattern = ".*csv$",
                              full.names = TRUE)
 
-df_files <- do.call(rbind.data.frame, as.list(csv_files)) %>% as_tibble()
-names(df_files)[1] <- "file_path"
-
-df_files <- df_files %>% 
-  mutate(stageRSS = str_extract(file_path, "\\s(\\S+)$") %>% str_replace(".csv", "") %>% trimws(),
+df_files <- do.call(rbind.data.frame, as.list(csv_files)) %>% 
+  as_tibble() %>% 
+  rename(file_path = names(.)[1]) %>% 
+  mutate(
+        stageRSS = str_extract(file_path, "\\s(\\S+)$") %>% str_replace(".csv", "") %>% trimws(),
+         
          stage = case_when(
            stageRSS == 1 ~ 1,
            stageRSS == 2 ~ 2,
@@ -47,11 +46,125 @@ df_files <- df_files %>%
            stageRSS == "3-2" ~ 4,
            stageRSS == 4 ~ 5,
            stageRSS == 5 ~ 6,
-           stageRSS == 6 ~ 7,
-         ))
+           stageRSS == 6 ~ 7),
+         
+         Etape = str_detect(file_path, "Etape") & 
+           str_detect(file_path, "Jeune", negate = TRUE),
+         
+         EtapeJeune = str_detect(file_path, "Etape") & 
+           str_detect(file_path, "Jeune"),
+         
+         General = str_detect(file_path, "General") & 
+           str_detect(file_path, "sorted", negate = TRUE) & 
+           str_detect(file_path, "Jeune", negate = TRUE),
+         
+         GeneralJeune = str_detect(file_path, "General") & 
+           str_detect(file_path, "sorted", negate = TRUE) & 
+           str_detect(file_path, "Jeune"),
+         
+         Equipe = str_detect(file_path, "Teams") &
+           stageRSS != "Equipes" ,
+         
+         Points = str_detect(file_path, "Sprint"),
+         
+         KOM = str_detect(file_path, "grimpeur"),
+         
+         ListeCoureurs = stageRSS == "Coureurs",
+         
+         ListeEquipes = stageRSS == "Equipes",
+         
+         ) %>% 
+  pivot_longer( !c(file_path, stageRSS, stage),
+                names_to = "classification",
+               )%>% 
+  filter(value) %>% 
+  select(-value) %>% 
+  arrange(classification)
 
+################################################################################ 
 
 # Liste des étapes présentes en csv
-distinct(df_files, stage) %>% drop_na() %>% arrange(stage)
+etapes_distinctes_csv <- distinct(df_files, stage) %>% drop_na() %>% arrange(stage)
 
 ################################################################################
+################################################################################ 
+
+# Fonction pour trouvée la ligne du CSV contenant les noms de colonnes
+
+# class : classification pour trouver le bon fichier csv
+# motCle : mot se retrouvant seulement dans les noms de colonnes
+
+################################################################################ 
+
+nb_lignes_skip <- function(class, motCle){
+  
+  # Ouvrir la connection vers le fichier
+  fc =  df_files %>% 
+    filter(classification == class) %>% 
+    pull(file_path) %>% 
+    file(open = "r", encoding = "UTF-16LE")
+  
+  rangee_titre = 0 # initialisation
+ 
+  # Trouver la ligne correspondante aux titres de colonnes
+  while(length( (l <- readLines(fc, n = 1) ) > 0 )){ 
+    
+    rangee_titre = rangee_titre + 1
+    
+    is_detected <- str_detect(l, fixed(motCle))
+    
+    if(is_detected) {break}
+    
+  }
+  
+  return(rangee_titre)
+}
+
+################################################################################ 
+################################################################################ 
+
+
+# Création de la liste des coureurs
+
+class <- "ListeCoureurs"
+
+path_liste_coureurs <- df_files %>% 
+  filter(classification == class) %>% 
+  pull(file_path)
+
+
+start_list_coureurs <- read.csv(path_liste_coureurs, 
+         sep = ';', 
+         skip = nb_lignes_skip(class, motCle = 'License'),
+         header = FALSE,
+         fileEncoding = "UTF-16LE") %>% 
+  as_tibble() %>% 
+  select(-V9) %>% 
+  drop_na() %>% 
+  rename( 
+    Bib = V1,
+    PremAnnee = V2,
+    Coureur = V3,
+    Team = V4,
+    TeamShort = V5,
+    License = V6,
+    Pays = V7,
+    Naissance = V8) %>% 
+  mutate(
+    Jeune = str_detect(PremAnnee, "#"),
+    Team = as_factor(Team),
+    TeamShort = as_factor(TeamShort),
+    Pays = as_factor(Pays),
+    Naissance = as_factor(Naissance)
+  ) %>% 
+  select(-PremAnnee)
+
+################################################################################ 
+
+# Création de la liste des équipes
+start_list_equipes <- start_list_coureurs %>% 
+  mutate(BibCategory = 10  * Bib %/% 10 ) %>% 
+  select(Team, TeamShort, BibCategory) %>% 
+  unique()
+  
+################################################################################ 
