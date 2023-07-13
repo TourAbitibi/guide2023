80322846994b08f2b9a570163731094fa55028ea
Bruno <bruno.gauthier@gmail.com>
2023-07-12 22:06:18 -0400

    Ajout résultats 2e étape

--- code/DocCourse.R
+++ code/DocCourse.R
@@ -15,15 +15,15 @@ ui_info("L'heure est {ui_value(now())}")
 #
 # Donnée étape à renseigner une fois
 
-Etape <- 1
+Etape <- 4
 
 HEURE_DEP_REEL <- heure_dep_fx(
   heure = 16,  
-  min = 50,
-  sec = 00)
+  min = 24,
+  sec = 20)
 
 # Données à MAJ en cours d'étape
-KM_ACTUEL <- 80.6
+KM_ACTUEL <- 1
 
 ################################################################################ 
 
